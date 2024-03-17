373c946fa23d776540f36593a6bacb1def1cd02b
zbrnvirgie <163149245+zbrnvirgie@users.noreply.github.com>
2024-03-18 00:50:54 +0700

    Add files via upload

---
+++ tpmod5_1302223055/tpmod5_1302223055/HaloGeneric.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HaloGeneric
+{
+    internal class HaloGeneric<L>
+    {
+        private L data;
+        public HaloGeneric(L data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Halo " + data);
+        }
+    }
+}
---
+++ tpmod5_1302223055/tpmod5_1302223055/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HaloGeneric;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        HaloGeneric<String> data = new HaloGeneric<string>("1302223055");
+        data.PrintData();
+
+    }
+}
